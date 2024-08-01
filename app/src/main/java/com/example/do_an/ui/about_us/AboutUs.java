427e7241433fb21c3c36dea4e9baeb6ea7f9eeb7
Johnny The child BEaTer <khainqia@gmail.com>
2024-08-01 17:24:18 +0700

    Push code CME212 finalized backEnd and frontEnd

--- app/src/main/java/com/example/do_an/ui/about_us/AboutUs.java
+++ app/src/main/java/com/example/do_an/ui/about_us/AboutUs.java
@@ -19,17 +19,6 @@ public class AboutUs extends Fragment {
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about_us, container, false);
-
-        TextView textView = view.findViewById(R.id.about_us_text1);
-        String text = "This app was developed by five students of the last year of FPT University. " +
-                "\n\nThe institution’s staunch commitment to creating a nurturing learning environment and stimulating intellectual curiosity.";
-        textView.setText(text);
-
-        textView = view.findViewById(R.id.about_us_text2);
-        text = "This application was built to detect malicious APK files and should be useful for searching for possible malware on Android devices.";
-        textView.setText(text);
-
-
         return view;
     }
 }
